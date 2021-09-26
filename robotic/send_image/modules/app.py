e8ff6cba8fbd883d73e55ea6356f4d2618a0ce09
Irvyn Cornejo <irvyncornejo@gmail.com>
2021-09-25 23:15:29 -0500

    Interfaz para uso de hardware

--- robotic/send_image/modules/app.py
+++ robotic/send_image/modules/app.py
@@ -9,18 +9,18 @@ class EnvioImagen:
     def __init__(self):
         self.url = 'https://w7a2uoj4wl.execute-api.us-west-2.amazonaws.com/test-dev/upload'
 
-    def __convertir_imagen(self, path='1.png'):
+    def __convertir_imagen(self, path):
         image = open(path, 'rb')
         image_read = image.read()
         image_64_encode = base64.encodestring(image_read)
         return image_64_encode.decode('ascii')
 
-    def enviar_imagen(self):
-        content = self.__convertir_imagen()
+    def enviar_imagen(self, path):
+        content = self.__convertir_imagen(path)
         
         body = {
             'autonomo': True,
-            'name': 'python.png',
+            'name': path,
             'content': content
         }
 
@@ -32,8 +32,3 @@ class EnvioImagen:
         r = requests.post(self.url, headers=header, data=json.dumps(body))
         
         return r.json()
-
-
-if __name__ == '__main__':
-    respuesta = EnvioImagen().enviar_imagen()
-    logging.info(respuesta['body']['elementos_encontrados'])
